356f6f331fd2efc62292c66c0225bdfa6a0719fc
KianZahrai <68393093+Kianzahrai@users.noreply.github.com>
2022-09-24 16:44:21 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,43 @@
+# Lab Attendance Checker :heavy_check_mark:
+## What does it do?
+
+### 1) Checks which students skipped the lab
+Compares a csv file of participants to 
+the csv file of students in enrolled in the lab section. 
+Students in the lab section that are not listed in the
+participants did not attend the lab.
+
+### 2) Matches Zoom user names to students' real names
+Some students forget to change their zoom name to their full name.
+This program tries to match ambiguous students to their
+real names.
+
+## How do I use this?
+
+1) Download the participants list csv file from zoom 
+([instructions](https://support.zoom.us/hc/en-us/articles/360039017432-Dashboard-for-meetings-and-webinars))
+(see formatting guidelines). 
+
+2) Add that csv file to the `student_files/participants` directory. Ensure that this file is the 
+*only* file in that directory. Participant files from previous labs can be kept in the 
+`student_files/participants/previous` directory.  
+
+3) Obtain a csv file that lists all the students in the lab section 
+(see formatting guidelines). 
+
+4) Put the csv file (see formatting guidelines) of all students in the lab section in the `student_files/all_students`
+directory. Ensure that this file is the *only* file in that directory.
+
+5) Run `app.py`!
+
+### CSV File Formatting Guidelines
+#### Both files
+No blank lines unless it's a single blank line at the beginning or end of the file.
+#### Participants file  
+First line: `Name (Original Name),User Email,Total Duration (Minutes),Guest`  
+Next lines: comma-separated values with each comma-separated value corresponding to a column 
+(Zoom should take care of this formatting)  
+#### All students file  
+Students' full names column: first non digit column, values are in the form
+`"<last_name>,<first_name>"` or `"<last_name>,<first_name> <middle_name>"` 
+TODO
---
+++ app.py
@@ -0,0 +1,149 @@
+import re
+import os
+import glob
+
+
+def open_file(file_path):
+    '''
+    (String) => String
+    opens and reads file
+    '''
+
+    # exception handling!!
+    return open(file_path)
+
+
+def get_names_from_file(file):
+    '''
+    (file object) => String[]
+    cleans file so that only the names remain
+    '''
+    file_lines = file.read().splitlines()
+    student_names = []
+
+    for i in range(0, len(file_lines)):
+        if i == 0:
+            continue
+        line = file_lines[i]
+        # https://stackabuse.com/regex-splitting-by-character-unless-in-quotes/
+        split_line = re.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", line)
+        is_participants_file = len(split_line[0]) != 0
+        if is_participants_file:
+            student_names.append(split_line[0].lower())
+        else:
+            student_names.append(get_full_name(split_line[2][1:len(split_line[2])-1].lower()))
+    return student_names
+
+
+def get_full_name(full_name):
+    '''
+    (str) -> str
+    Takes full_name in the form '"<last_name>, <first_name>"' then returns
+    the name in the form '<first_name> <last_name>'
+    '''
+    last_first = full_name.split(",")
+    first_name = last_first[1].split(" ")[0]
+    return first_name + " " + last_first[0]
+
+
+def not_attended(participants):
+    '''
+    str[] => str[]
+    Compares list of participants to list of all students in the lab section
+    returns a list of the students who did not attend the lab
+    '''
+    result = []
+
+    real_participants = []
+    print("\nZOOM NAME => REAL NAME")
+    for p in participants:
+        real_participants.append(find_real_name(p))
+    real_participants.sort()
+
+    for real_stu in all_students:
+        if binary_search(real_participants, real_stu) == -1:
+            result.append(real_stu)
+    return result
+
+
+def find_real_name(user_name):
+    '''
+    string => string
+    Determines a student's real name based on their zoom name
+    '''
+    results = []
+    n = len(user_name)
+    # print("\n"+user_name)
+    for real_stu in all_students:
+        if user_name == real_stu:
+            return real_stu
+        for i in range(n-2):
+            for j in range(i+3, n+1):
+                substr = user_name[i:j]
+                if " " in substr: continue
+                if substr in real_stu:
+                    match_points = 0
+                    if binary_search(real_stu.split(" "), substr) != -1:
+                        match_points += 1
+                    if real_stu.find(substr) == 0:
+                        match_points += 1
+                    if binary_search(real_stu.split(" "), substr) != -1 and real_stu.find(substr) == 0:
+                        match_points += 1
+                    if len(results) == 0 or len(substr) > int(results[-1][1]) \
+                            or (len(substr) >= int(results[-1][1]) and match_points > results[-1][2]):
+                        # print(substr)
+                        if len(results) != 0 and results[-1][2] == match_points:
+                            continue
+                        results.append([real_stu, len(substr), match_points])
+                        # print(results)
+
+    match = results[-1][0] if len(results) != 0 else "NO MATCH"
+    if user_name != match:
+        print(f"{user_name} => {match}")
+    return match
+
+
+# https://www.geeksforgeeks.org/python-program-for-binary-search/
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+
+    while low <= high:
+        mid = (high + low) // 2
+
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+    return -1
+
+
+# main
+
+current_dir = os.path.dirname(__file__)
+participants_dir = os.path.join(current_dir, os.path.join("student_files", "participants"))
+participants_file_path = glob.glob(participants_dir+"/*.csv")[0]
+all_students_dir = os.path.join(current_dir, os.path.join("student_files", "all_students"))
+all_students_file_path = glob.glob(all_students_dir+"/*.csv")[0]
+print(f"participants_file_path: {participants_file_path}")
+print(f"all_students_file_path: {all_students_file_path}")
+
+participants = open_file(participants_file_path)
+all_students_orig = open_file(all_students_file_path)
+
+part_names = get_names_from_file(participants)
+all_students = sorted(get_names_from_file(all_students_orig))
+
+part_names_sorted = sorted(part_names)
+
+print("\nPARTICIPANTS")
+print(part_names_sorted)
+print("\nALL STUDENTS")
+print(all_students)
+
+not_in_lab = not_attended(part_names_sorted)
+print("\nNOT IN LAB")
+print(not_in_lab)
